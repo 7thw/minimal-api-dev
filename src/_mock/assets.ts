39a6626c0c8446d5ba90ead2e484dbe4c3617e99
hamigg <h@7thw.com>
2023-11-15 15:44:44 -0500

    Jayvion Api

--- src/_mock/assets.ts
+++ src/_mock/assets.ts
@@ -92,7 +92,7 @@ export const _emails = [
 // ----------------------------------------------------------------------
 
 export const _fullNames = [
-  'Jayvion Simonn',
+  'Jayvion Api',
   'Lucian Obrien',
   'Deja Brady',
   'Harrison Stein',
